c171d7e2169c273d4b18759a124e044139bfb59d
rproskuryakov <rodion43pv@gmail.com>
2019-08-04 13:53:20 +0700

    Реализована функция get_train_data с проверкой на пустоту массива

--- model/preprocessor.py
+++ model/preprocessor.py
@@ -3,4 +3,28 @@ import numpy as np
 
 def get_train_data(array, window=5, 
                    n_days_forward=7):
-    return np.array([])
+    """
+    Функция, преобразующая временной ряд в двумерный массив для линейной регрессии.
+
+    :param array: одномерный массив, содержащий значения временного ряда
+    :param window: длина скользящего окна
+    :param n_days_forward: число дней, значения в которые мы предсказываем
+    :return: numpy.array с .shape = (n_days_forward, window)
+    """
+    length = len(array)
+    if not length:
+        raise ValueError('Empty array')
+    # if # проверка на window:
+    #     raise ValueError('Incorrect window size')
+    # if # проверка на n_days_forward:
+    #     raise ValueError('Incorrest n_days_forward')
+    # вставить условие проверки на удовлетворение размеров окна и n_days_forward
+    new_array = np.zeros((n_days_forward, window))
+    
+    for row in range(n_days_forward):
+        start = length - n_days_forward + row - window + 1
+        stop = start + window
+        new_array[row, :] = array[start:stop]
+    return new_array
+        
+
--- test/test.py
+++ test/test.py
@@ -2,48 +2,50 @@ import sys
 import unittest
 import numpy as np
 
-from model.preprocessor import get_train_data
-
 
 sys.path.append('../')
+from model.preprocessor import get_train_data
 
 
 class GetTrainDataTests(unittest.TestCase):
     def setUp(self):
-        self.array = np.array([0, 5, 3, 10, 4, 14,
-                               7, 9, 30, 53, 42, 44])
+        self.array_1 = np.array([0, 5, 3, 10, 4, 14,
+                                 7, 9, 30, 53, 42, 44])
 
     def test_get_train_data_1(self):
         """
         Проверка функции с параметрами window=2, n_days_forward=5
         """
-        train = get_train_data(self.array, window=2, n_days_forward=5)
+        train = get_train_data(self.array_1, window=2, n_days_forward=5)
         answer = np.array([[7, 9],
                            [9, 30],
                            [30, 53],
                            [53, 42],
                            [42, 44]])
-        self.assertEqual(train, answer)
+        self.assertTrue((train == answer).all())
 
     def test_get_train_data_2(self):
         """
         Проверка функции с параметрами window=1, n_days_forward=7
         """
-        train = get_train_data(self.array, window=1, n_days_forward=7)
+        train = get_train_data(self.array_1, window=1, n_days_forward=7)
         answer = np.array([[14], [7], [9], [30], [53], [42], [44]])
-        self.assertEqual(train, answer)
+        self.assertTrue((train == answer).all())
 
     def test_get_train_data_3(self):
         """
-        Проверка функции с пустым массивом на входе
+        Проверка функции с пустым массивом на входе. Пусть функция возвращает ValueError
         """
         array = np.array([])
-        train = get_train_data(array)
-        self.assertEqual(train, array)
+        with self.assertRaises(ValueError) as context:
+            get_train_data(array)
+
+        self.assertTrue('Empty array' in str(context.exception))
 
     def test_get_train_data_4(self):
         """
         Проверка поведения функции при завышенном window_size (параметр превышает размер массива)
+        Должен падать!
         """
         array = np.array([0, 5, 10, 4])
         train = get_train_data(array, window=5, n_days_forward=3)
@@ -54,6 +56,7 @@ class GetTrainDataTests(unittest.TestCase):
         """
         Проверка поведения функции при завышенном n_days_forward 
         (с заданным window_size не получается нагенерить нужное количество строк в матрице)
+        Должен падать!
         """
         array = np.array([0, 5, 10, 4])
         train = get_train_data(array, window=1, n_days_forward=7)
@@ -62,15 +65,20 @@ class GetTrainDataTests(unittest.TestCase):
 
     def test_get_train_data_6(self):
         """
-        Проверка функции с window_size=4, n_days_forward=5
+        Проверка функции с window_size=5, n_days_forward=5
+        array_1 = np.array([0, 5, 3, 10, 4, 14,
+                            7, 9, 30, 53, 42, 44])
+                            12 - 5 = 7 - window = 2 + 1
+                            start = 3 stop = start + 5(window)
+                            start = 7 stop = start + 5(window)
         """
-        train = get_train_data(self.array, window=4, n_days_forward=5)
+        train = get_train_data(self.array_1, window=5, n_days_forward=5)
         answer = np.array([[10, 4, 14, 7, 9],
                            [4, 14, 7, 9, 30],
                            [14, 7, 9, 30, 53],
                            [7, 9, 30, 53, 42],
                            [9, 30, 53, 42, 44]])
-        self.assertEqual(train, answer)
+        self.assertTrue((train == answer).all())
 
 
 
